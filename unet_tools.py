c2a2359bc4a080a0a02149f501ef07177f005bb0
Amanda Thomas <amthomas@uoregon.edu>
2020-09-29 10:52:21 -0700

    9/29 update

--- unet_tools.py
+++ unet_tools.py
@@ -117,6 +117,62 @@ def make_large_unet(fac, sr):
         model=tf.keras.models.Model(input_layer,output)
         opt = tf.keras.optimizers.Adam(lr=0.0001)
         model.compile(loss='binary_crossentropy',optimizer=opt,metrics=['accuracy'])
+        
+    if sr==250:
+        input_layer=tf.keras.layers.Input(shape=(120,2)) # 1 Channel seismic data
+      
+        #These Convolutional blocks expect 2D data (time-steps x channels)
+        #This is just one channel, but if you wanted to add more stations as extra channels you can
+        #network=tf.keras.layers.Reshape((600,2))(input_layer)
+        
+        # build the network, here is your first convolution layer
+        level1=tf.keras.layers.Conv1D(int(fac*32),21,activation='relu',padding='same')(input_layer) # N filters, Filter Size, Stride, padding
+        
+        # This layer is a trick of the trade it helps training deeper networks, by keeping gradients close to the same scale
+        #network=tf.keras.layers.BatchNormalization()(level1)
+        
+        #Max Pooling Layer
+        network=tf.keras.layers.MaxPooling1D()(level1) #60
+        
+        #Next Block
+        level2=tf.keras.layers.Conv1D(int(fac*64),15,activation='relu',padding='same')(network)
+        #network=tf.keras.layers.BatchNormalization()(level2)
+        network=tf.keras.layers.MaxPooling1D()(level2) #30
+        
+        #Next Block
+        level3=tf.keras.layers.Conv1D(int(fac*128),11,activation='relu',padding='same')(network)
+        #network=tf.keras.layers.BatchNormalization()(level3)
+        network=tf.keras.layers.MaxPooling1D()(level3) #15
+        
+        #Base of Network
+        network=tf.keras.layers.Flatten()(network)
+        base_level=tf.keras.layers.Dense(15,activation='relu')(network)
+        
+        #network=tf.keras.layers.BatchNormalization()(base_level)
+        network=tf.keras.layers.Reshape((15,1))(base_level)
+        
+        #Upsample and add skip connections
+        network=tf.keras.layers.Conv1D(int(fac*128),11,activation='relu',padding='same')(network) # N filters, Filter Size, Stride, padding
+        network=tf.keras.layers.UpSampling1D()(network)
+        level3=tf.keras.layers.Concatenate()([network,level3]) # N filters, Filter Size, Stride, padding
+        
+        #Upsample and add skip connections
+        network=tf.keras.layers.Conv1D(int(fac*64),15,activation='relu',padding='same')(level3) # N filters, Filter Size, Stride, padding
+        network=tf.keras.layers.UpSampling1D()(network)
+        level2=tf.keras.layers.Concatenate()([network,level2]) # N filters, Filter Size, Stride, padding
+        
+        #Upsample and add skip connections
+        network=tf.keras.layers.Conv1D(int(fac*32),21,activation='relu',padding='same')(level2) # N filters, Filter Size, Stride, padding
+        network=tf.keras.layers.UpSampling1D()(network)
+        level1=tf.keras.layers.Concatenate()([network,level1]) # N filters, Filter Size, Stride, padding
+        
+        #End of network
+        network=tf.keras.layers.Conv1D(1,21,activation='sigmoid',padding='same')(level1) # N filters, Filter Size, Stride, padding
+        output=tf.keras.layers.Flatten()(network) # N filters, Filter Size, Stride, padding
+        
+        model=tf.keras.models.Model(input_layer,output)
+        opt = tf.keras.optimizers.Adam(lr=0.0001)
+        model.compile(loss='binary_crossentropy',optimizer=opt,metrics=['accuracy'])
     
     model.summary()
     
